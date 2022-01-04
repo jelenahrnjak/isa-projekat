49af7a6cb6c70d998365a6afb8b88a7f893e41af
Jelena Hrnjak <jelenahrnjak99@gmail.com>
2022-01-04 22:25:42 +0100

    Feat/angular app setting
    * feat: added project
    
    * feat: modified front
    
    Co-authored-by: jelenahrnjak <jelenahrnjak@uns.ac.rs>

---
+++ WishAndFishFront/angular-app/src/app/home/home.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core'; 
+import {UserService} from '../service/user.service';
+import {ConfigService} from '../service/config.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  fooResponse = {};
+  whoamIResponse = {};
+  allUserResponse = {};
+
+  constructor(
+    private config: ConfigService, 
+    private userService: UserService
+  ) {
+  }
+
+  ngOnInit() {
+  }
+
+  makeRequest(path) {
+     
+  }
+
+  forgeResonseObj(obj, res, path) {
+    obj['path'] = path;
+    obj['method'] = 'GET';
+    if (res.ok === false) {
+      // err
+      obj['status'] = res.status;
+      try {
+        obj['body'] = JSON.stringify(JSON.parse(res._body), null, 2);
+      } catch (err) {
+        console.log(res);
+        obj['body'] = res.error.message;
+      }
+    } else {
+      // 200
+      obj['status'] = 200;
+      obj['body'] = JSON.stringify(res, null, 2);
+    }
+  }
+
+}
