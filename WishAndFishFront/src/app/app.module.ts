a52b30444b059a200a179271f46e7ab1b37f6331
Кристина Стојић <73244557+KristinaStojic@users.noreply.github.com>
2022-01-04 02:58:50 +0100

    Feat/adding bootstrap
    * feat: added bootstrap and angular material
    
    * feat: added empty services

--- WishAndFishFront/src/app/app.module.ts
+++ WishAndFishFront/src/app/app.module.ts
@@ -3,7 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+//import {AngularMaterialModule} from './angular-material/angular-material.module';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,12 @@ import { RegistrationComponent } from './registration/registration.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatTableModule
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,RegistrationComponent]
 })
 export class AppModule { }
