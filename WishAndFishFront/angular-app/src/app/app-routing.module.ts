5ee96dc8036aa61d7848b85a280696fe53a1d5bc
Kristina Stojic <kristinastojic99@uns.ac.rs>
2022-01-04 23:16:51 +0100

    fix: path for components

--- WishAndFishFront/angular-app/src/app/app-routing.module.ts
+++ WishAndFishFront/angular-app/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
