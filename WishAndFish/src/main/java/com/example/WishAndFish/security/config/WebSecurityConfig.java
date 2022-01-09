6f614e90f7e6b79465219fa3ed712beb48f0f37f
Kristina Stojic <kristinastojic99@uns.ac.rs>
2022-01-09 09:17:41 +0100

    feat: implemented boats from owner view - back

--- WishAndFish/src/main/java/com/example/WishAndFish/security/config/WebSecurityConfig.java
+++ WishAndFish/src/main/java/com/example/WishAndFish/security/config/WebSecurityConfig.java
@@ -83,6 +83,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/users/requestDeleting").permitAll()
                 .antMatchers("/api/cottageOwner/**").permitAll()
                 .antMatchers("/api/cottages/**").permitAll()
+                .antMatchers("/api/boatOwner/**").permitAll()
 
                 // ukoliko ne zelimo da koristimo @PreAuthorize anotacije nad metodama kontrolera, moze se iskoristiti hasRole() metoda da se ogranici
                 // koji tip korisnika moze da pristupi odgovarajucoj ruti. Npr. ukoliko zelimo da definisemo da ruti 'admin' moze da pristupi
--- WishAndFish/src/main/java/com/example/WishAndFish/service/BoatOwnerService.java
+++ WishAndFish/src/main/java/com/example/WishAndFish/service/BoatOwnerService.java
@@ -1,12 +1,17 @@
 package com.example.WishAndFish.service;
 
 import com.example.WishAndFish.dto.AddressDTO;
+import com.example.WishAndFish.dto.BoatDTO;
+import com.example.WishAndFish.dto.CottageDTO;
 import com.example.WishAndFish.dto.UserDTO;
 import com.example.WishAndFish.model.*;
 import com.example.WishAndFish.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BoatOwnerService {
@@ -28,6 +33,9 @@ public class BoatOwnerService {
     @Autowired
     private BoatOwnerRepository boatOwnerRepository;
 
+    @Autowired
+    private BoatRepository boatRepository;
+
     public User save(UserDTO requestUser) {
         AddressDTO a = requestUser.getAddress();
         BoatOwner user = new BoatOwner(passwordEncoder.encode(requestUser.getPassword()), requestUser.getEmail(), requestUser.getName(), requestUser.getSurname(), requestUser.getPhoneNumber());
@@ -41,5 +49,16 @@ public class BoatOwnerService {
         user.setRole(role);
 
         return this.boatOwnerRepository.save(user);
+    }
+
+    public List<BoatDTO> getBoatsFromOwner(String email){
+        List<BoatDTO> ret = new ArrayList<BoatDTO>();
+        for(Boat b: boatRepository.findAll()){
+            User u = userRepository.findByEmail(b.getBoatOwner().getEmail());
+            if(u.getEmail().equals(email)){
+                ret.add(new BoatDTO(b));
+            }
+        }
+        return  ret;
     }
 }
