b8f74faaa94b0c44c8bda337f1a2ec7d9e85c62c
jelena.hrnjak <jelena.hrnjak@synechron.com>
2022-04-09 01:30:41 +0200

    feat: changed model and added client

---
+++ WishAndFish/src/main/java/com/example/WishAndFish/model/Client.java
@@ -0,0 +1,26 @@
+package com.example.WishAndFish.model;
+
+import lombok.*;
+
+import javax.persistence.CascadeType;
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Client extends User{
+
+    @ManyToMany(targetEntity = Cottage.class, cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    private Set<Cottage> cottageSubscriptions;
+
+    @ManyToMany(targetEntity = Boat.class, cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    private Set<Boat> boatSubscriptions;
+
+    @ManyToMany(targetEntity = FishingAdventure.class, cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    private Set<FishingAdventure> adventureSubscriptions;
+}
+
+
--- WishAndFish/src/main/java/com/example/WishAndFish/model/User.java
+++ WishAndFish/src/main/java/com/example/WishAndFish/model/User.java
@@ -5,11 +5,15 @@ import java.sql.Timestamp;
 import java.util.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import static javax.persistence.InheritanceType.TABLE_PER_CLASS;
 
+@Getter
+@Setter
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -21,82 +25,53 @@ public class User implements UserDetails{
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "password", unique = false, nullable = false)
+    @Column(name = "password", nullable = false)
     private String password;
+
     @Column(name = "email", unique = true, nullable = false)
     private String email;
-    @Column(name = "name", unique = false, nullable = false)
+
+    @Column(name = "name", nullable = false)
     private String name;
-    @Column(name = "surname", unique = false, nullable = false)
-    private String surname; 
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
     @OneToOne(targetEntity = Address.class,cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id")
     private Address address;
-    @Column(name = "phoneNumber", unique = false, nullable = false)
+
+    @Column(name = "phoneNumber", nullable = false)
     private String phoneNumber;
-    @Column(name = "deleted", unique = false, nullable = false)
+
+    @Column(name = "deleted", nullable = false)
     private boolean deleted;
-    @Column(name = "enabled", unique = false, nullable = false)
+
+    @Column(name = "enabled", nullable = false)
     private boolean enabled;
-    @Column(name = "points", unique = false, nullable = false)
+
+    @Column(name = "points", nullable = false)
     private double points;
+
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
     @JoinColumn(name = "loyalty_category_id", nullable = false)
     private LoyaltyCategory loyaltyCategory;
+
     @Column(name = "last_password_reset_date")
     private Timestamp lastPasswordResetDate;
+
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
     @JoinColumn(name = "role_id")
     private Role role;
+
+
     @Column(name = "verification_code", length = 64)
     private String verificationCode;
+
     @Column(name = "reason_for_registration")
     private String reasonForRegistration;
     public User() {
         super();
-    }
-
-    public User(String password, String email, String name, String surname, String phoneNumber) {
-        this.password = password;
-        this.email = email;
-        this.name = name;
-        this.surname = surname;
-        this.phoneNumber = phoneNumber;
-        this.deleted = false;
-        this.enabled = false;
-        this.points = 0.0;
-    }
-
-    public User(User u){
-        this.password = u.getPassword();
-        this.email = u.getEmail();
-        this.name = u.getName();
-        this.surname = u.getSurname();
-        this.phoneNumber = u.getPhoneNumber();
-        this.deleted = u.isDeleted();
-        this.enabled = u.isEnabled();
-        this.points = u.getPoints();
-        this.loyaltyCategory = u.getLoyaltyCategory();
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setPassword(String password) {
@@ -106,107 +81,10 @@ public class User implements UserDetails{
         this.password = password;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
 
     @Override
     public boolean isEnabled() {
         return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public double getPoints() {return points; }
-
-    public void setPoints(double points) {
-        this.points = points;
-//        if(points > 500) {
-//            this.loyaltyCategory = LoyaltyCategories.SILVER;
-//            this.discount = 5;
-//        }
-//        if(points > 1500) {
-//            this.discount = 15;
-//            this.loyaltyCategory = LoyaltyCategories.GOLD;
-//        }
-    }
-
-    public LoyaltyCategory getLoyaltyCategory() { return loyaltyCategory; }
-
-    public void setLoyaltyCategory(LoyaltyCategory loyaltyCategory) { this.loyaltyCategory = loyaltyCategory; }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public String getVerificationCode() {
-        return verificationCode;
-    }
-
-    public void setVerificationCode(String verificationCode) {
-        this.verificationCode = verificationCode;
-    }
-
-    public Timestamp getLastPasswordResetDate() {
-        return lastPasswordResetDate;
-    }
-
-    public void setLastPasswordResetDate(Timestamp lastPasswordResetDate) {
-        this.lastPasswordResetDate = lastPasswordResetDate;
-    }
-
-    public String getReasonForRegistration() {
-        return reasonForRegistration;
-    }
-
-    public void setReasonForRegistration(String reasonForRegistration) {
-        this.reasonForRegistration = reasonForRegistration;
     }
 
     @Override
@@ -253,5 +131,30 @@ public class User implements UserDetails{
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
+    }
+
+
+
+    public User(String password, String email, String name, String surname, String phoneNumber) {
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+        this.deleted = false;
+        this.enabled = false;
+        this.points = 0.0;
+    }
+
+    public User(User u){
+        this.password = u.getPassword();
+        this.email = u.getEmail();
+        this.name = u.getName();
+        this.surname = u.getSurname();
+        this.phoneNumber = u.getPhoneNumber();
+        this.deleted = u.isDeleted();
+        this.enabled = u.isEnabled();
+        this.points = u.getPoints();
+        this.loyaltyCategory = u.getLoyaltyCategory();
     }
 }
