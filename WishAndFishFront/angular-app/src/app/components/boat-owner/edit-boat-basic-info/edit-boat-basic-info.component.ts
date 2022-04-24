cf18cf912e063a09f9e2cee216bab54c8776f036
Kristina Stojic <stojic.kris@gmail.com>
2022-04-24 23:11:19 +0200

    fix: validation for editing boat - check if fields contain letters

--- WishAndFishFront/angular-app/src/app/components/boat-owner/edit-boat-basic-info/edit-boat-basic-info.component.ts
+++ WishAndFishFront/angular-app/src/app/components/boat-owner/edit-boat-basic-info/edit-boat-basic-info.component.ts
@@ -35,6 +35,8 @@ export class EditBoatBasicInfoComponent implements OnInit {
 
   submit(){
 
+    var letters = /[a-zA-Z]/;
+
     if(this.boat.name == "" || this.boat.type == "" || this.boat.pricePerHour == "" || this.boat.description == "" || this.boat.cancellationConditions == "" || this.boat.maximumPeople == "" || this.boat.length == "" || this.boat.engineNumber == "" || this.boat.enginePower == "" || this.boat.maxSpeed == "" || this.boat.capacity == ""
     || this.boat.address.cityName == "" || this.boat.address.cityName == "" || this.boat.address.latitude == "" || this.boat.address.longitude == "" || this.boat.address.postalCode == "" || this.boat.address.street == "" || this.boat.address.streetNumber == ""){
       Swal.fire({
@@ -43,6 +45,13 @@ export class EditBoatBasicInfoComponent implements OnInit {
         text: 'Fill all fields!',
       })    
       console.log(this.boat)
+    }
+    else if(letters.test(this.boat.pricePerHour) || letters.test(this.boat.length) || letters.test(this.boat.engineNumber) || letters.test(this.boat.enginePower) || letters.test(this.boat.maxSpeed) || letters.test(this.boat.capacity) || letters.test(this.boat.address.latitude) || letters.test(this.boat.address.longitude)){
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Fill in the fields correctly, some of them should not contain letters!',
+      })  
     }
     else{
       this.boatService.editBasicInfo(this.boat).subscribe((data) =>{
