d470d3055fe1e7fb81055b5fb08205c93b5b26e4
Kristina Stojic <stojic.kris@gmail.com>
2022-05-31 03:29:33 +0200

    feat: added map to the adding boat

--- WishAndFishFront/angular-app/src/app/components/boat-owner/add-boat/add-boat.component.ts
+++ WishAndFishFront/angular-app/src/app/components/boat-owner/add-boat/add-boat.component.ts
@@ -1,3 +1,4 @@
+import { Boat } from 'src/app/model/boat.model';
 import { BoatService } from 'src/app/service/boat.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -5,6 +6,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService, UserService } from '../../../service';
 import { Subject } from 'rxjs/Subject';
 import { takeUntil } from 'rxjs/operators';
+import Swal from 'sweetalert2';
+import { MapsAPILoader, MouseEvent } from '@agm/core';
 
 interface DisplayMessage {
   msgType: string;
@@ -23,7 +26,20 @@ export class AddBoatComponent implements OnInit {
   returnUrl: string;
   private ngUnsubscribe: Subject<void> = new Subject<void>();
   selectedFile = null;
+  lat = 45.23731467405363;
+  lng = 19.842083286315937;
 
+  private geoCoder;
+  address: string;
+  zoom: number;
+
+
+  longitude: number = 0;
+  latitude: number = 0;
+
+
+  boat = new Boat()
+  
   constructor(private userService: UserService,
     private authService: AuthService,
     private router: Router,
@@ -32,6 +48,9 @@ export class AddBoatComponent implements OnInit {
     private boatService: BoatService) { }
 
   ngOnInit() {
+    this.geoCoder = new google.maps.Geocoder();
+
+
     this.route.params
     .pipe(takeUntil(this.ngUnsubscribe))
     .subscribe((params: DisplayMessage) => {
@@ -52,13 +71,13 @@ export class AddBoatComponent implements OnInit {
       description : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
       capacity : ['',Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+)$')])],
       // pricePerHour : ['',Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+\.?[0-9]*|\.[0-9]+)$')])],
-      street: ['', Validators.compose([Validators.required, Validators.minLength(0)])],
-      streetNumber : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
-      postalCode : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
-      longitude : ['',Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+\.?[0-9]*|\.[0-9]+)$')])],
-      latitude : ['',Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+\.?[0-9]*|\.[0-9]+)$')])],
-      cityName : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
-      countryName : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
+      // street: ['', Validators.compose([Validators.required, Validators.minLength(0)])],
+      // streetNumber : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
+      // postalCode : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
+      // longitude : ['',Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+\.?[0-9]*|\.[0-9]+)$')])],
+      // latitude : ['',Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+\.?[0-9]*|\.[0-9]+)$')])],
+      // cityName : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
+      // countryName : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
       cancellationConditions: ['', Validators.compose([Validators.required, Validators.minLength(0)])],
     })
   }
@@ -77,20 +96,20 @@ export class AddBoatComponent implements OnInit {
       "pricePerDay" : this.form.get('price').value,
 
       "address" : {
-          "street" : this.form.get('street').value,
-          "streetNumber" : this.form.get('streetNumber').value,
-          "postalCode" : this.form.get('postalCode').value,
-          "longitude" : this.form.get('longitude').value,
-          "latitude" : this.form.get('latitude').value, 
-          "cityName" : this.form.get('cityName').value,
-          "countryName" : this.form.get('countryName').value    
-      },
+        "street" : this.boat.address.street,
+        "streetNumber" : this.boat.address.streetNumber,
+        "postalCode" : this.boat.address.postalCode,
+        "longitude" : this.longitude,
+        "latitude" : this.latitude,
+        "cityName" : this.boat.address.cityName,
+        "countryName" : this.boat.address.countryName   
+   },
       "ownerEmail" : localStorage.getItem("user"),
       "cancellationConditions" : this.form.get('cancellationConditions').value,
       "coverImage": this.selectedFile
   }
 
-  if(this.form.valid){
+  if(this.form.valid  && this.longitude !== 0 && this.latitude !== 0){
       this.boatService.addBoat(boat)
     .subscribe(data => {
       this.router.navigate([this.returnUrl]);
@@ -100,6 +119,12 @@ export class AddBoatComponent implements OnInit {
       
       });
       console.log(boat);
+  } else{
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Select addrress!',
+    })
   }
   
   }
@@ -107,5 +132,61 @@ export class AddBoatComponent implements OnInit {
   selectImage(event){
     this.selectedFile = event.target.files[0].name;
   }
+
+
+
+  
+  markerDragEnd($event: MouseEvent) {
+    console.log($event.coords.lat)
+    console.log($event.coords.lng)
+
+    this.latitude= $event.coords.lat
+    this.longitude = $event.coords.lng
+
+    this.getAddress($event.coords.lat, $event.coords.lng);
+    console.log(this.boat)
+  }
+
+
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      //console.log(results);
+      //console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+          console.log(results[0])
+
+          if(results[0].address_components.filter(ac=>~ac.types.indexOf('locality')) != undefined){
+            this.boat.address.cityName = results[0].address_components.filter(ac=>~ac.types.indexOf('locality'))[0].long_name
+          }
+
+          if(results[0].address_components.filter(ac=>~ac.types.indexOf('country')) != undefined){
+            this.boat.address.countryName = results[0].address_components.filter(ac=>~ac.types.indexOf('country'))[0].long_name
+          }
+
+          if( results[0].address_components.filter(ac=>~ac.types.indexOf('street_number')) != undefined){
+            this.boat.address.streetNumber = results[0].address_components.filter(ac=>~ac.types.indexOf('street_number'))[0].long_name
+          }
+
+          if(results[0].address_components.filter(ac=>~ac.types.indexOf('route')) != undefined){
+            this.boat.address.street = results[0].address_components.filter(ac=>~ac.types.indexOf('route'))[0].long_name
+          }
+
+          if(results[0].address_components.filter(ac=>~ac.types.indexOf('postal_code')) != undefined){
+            this.boat.address.postalCode = results[0].address_components.filter(ac=>~ac.types.indexOf('postal_code'))[0].long_name
+          }
+
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
+  }
+
 
 }
