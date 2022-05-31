a2ed7ecaa5cf2e7869f21b60fa887a53d66dd773
Kristina Stojic <stojic.kris@gmail.com>
2022-05-31 03:14:07 +0200

    feat: added map to adding cottage

--- WishAndFishFront/angular-app/src/app/components/cottage-owner/add-cottage/add-cottage.component.ts
+++ WishAndFishFront/angular-app/src/app/components/cottage-owner/add-cottage/add-cottage.component.ts
@@ -1,3 +1,5 @@
+import { Cottage } from './../../../model/cottage';
+import { CommentService } from './../../../service/comment.service';
 import { CottageService } from 'src/app/service/cottage.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -5,6 +7,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService, UserService } from '../../../service';
 import { Subject } from 'rxjs/Subject';
 import { takeUntil } from 'rxjs/operators';
+import { MapsAPILoader, MouseEvent } from '@agm/core';
+import Swal from 'sweetalert2';
 
 interface DisplayMessage {
   msgType: string;
@@ -25,8 +29,21 @@ export class AddCottageComponent implements OnInit {
   formAdress : FormGroup;
   notification: DisplayMessage;
   selectedFile = null;
-
+  lat = 45.23731467405363;
+  lng = 19.842083286315937;
   returnUrl: string;
+  private geoCoder;
+  address: string;
+  zoom: number;
+
+
+  longitude: number = 0;
+  latitude: number = 0;
+
+
+  cottage = new Cottage()
+  
+
   private ngUnsubscribe: Subject<void> = new Subject<void>();
   constructor( private userService: UserService,
     private authService: AuthService,
@@ -36,6 +53,10 @@ export class AddCottageComponent implements OnInit {
     private cottageService: CottageService) { }
 
   ngOnInit() {
+    this.geoCoder = new google.maps.Geocoder();
+
+
+
     this.route.params
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe((params: DisplayMessage) => {
@@ -51,13 +72,13 @@ export class AddCottageComponent implements OnInit {
         rooms: ['' ,Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+)$')])],
         beds: ['' ,Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+)$')])],
         price: ['' ,Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+)$')])],
-        street: ['', Validators.compose([Validators.required, Validators.minLength(0)])],
-        streetNumber : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
-        postalCode : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
-        longitude : ['',Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+\.?[0-9]*|\.[0-9]+)$')])],
-        latitude : ['',Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+\.?[0-9]*|\.[0-9]+)$')])],
-        cityName : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
-        countryName : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
+        // street: ['', Validators.compose([Validators.required, Validators.minLength(0)])],
+        // streetNumber : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
+        // postalCode : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
+        // longitude : ['',Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+\.?[0-9]*|\.[0-9]+)$')])],
+        // latitude : ['',Validators.compose([Validators.required, Validators.min(0), Validators.pattern('([0-9]+\.?[0-9]*|\.[0-9]+)$')])],
+        // cityName : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
+        // countryName : ['', Validators.compose([Validators.required, Validators.minLength(0)])],
        
       })
   }
@@ -79,6 +100,14 @@ export class AddCottageComponent implements OnInit {
 
   onSubmit() {
     this.notification = undefined;
+    // this.cottage.name = this.form.get('name').value
+    // this.cottage.description = this.form.get('description').value
+    // this.cottage.numberOfRooms = this.form.get('numberOfRooms').value
+    // this.cottage.bedsPerRoom = this.form.get('bedsPerRoom').value
+    // this.cottage.price = this.form.get('price').value
+    // this.cottage.ownerEmail = this.form.get('price').value
+    // this.cottage.price = this.form.get('price').value
+
     var Cottage = { 
       "name" : this.form.get('name').value,
       "description" : this.form.get('description').value,
@@ -86,20 +115,20 @@ export class AddCottageComponent implements OnInit {
       "bedsPerRoom":  this.form.get('beds').value,
       "price" : this.form.get('price').value,
       "address" : {
-          "street" : this.form.get('street').value,
-          "streetNumber" : this.form.get('streetNumber').value,
-          "postalCode" : this.form.get('postalCode').value,
-          "longitude" : this.form.get('longitude').value,
-          "latitude" : this.form.get('latitude').value, 
-          "cityName" : this.form.get('cityName').value,
-          "countryName" : this.form.get('countryName').value    
+           "street" : this.cottage.address.street,
+           "streetNumber" : this.cottage.address.streetNumber,
+           "postalCode" : this.cottage.address.postalCode,
+           "longitude" : this.longitude,
+           "latitude" : this.latitude,
+           "cityName" : this.cottage.address.cityName,
+           "countryName" : this.cottage.address.countryName   
       },
       "ownerEmail" : localStorage.getItem("user"),
       "coverImage": this.selectedFile
 
   }
 
-  if(this.form.valid){
+  if(this.form.valid && this.longitude !== 0 && this.latitude !== 0){
 
     this.cottageService.addCottage(Cottage)
     .subscribe(data => {
@@ -111,11 +140,72 @@ export class AddCottageComponent implements OnInit {
       });
       console.log(Cottage);
     }
+    else{
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Select addrress!',
+      })
+    }
+
   }
 
 
   selectImage(event){
     this.selectedFile = event.target.files[0].name;
+  }
+
+
+  markerDragEnd($event: MouseEvent) {
+    console.log($event.coords.lat)
+    console.log($event.coords.lng)
+
+    this.latitude= $event.coords.lat
+    this.longitude = $event.coords.lng
+
+    this.getAddress($event.coords.lat, $event.coords.lng);
+    console.log(this.cottage)
+  }
+
+
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      //console.log(results);
+      //console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+          console.log(results[0])
+
+          if(results[0].address_components.filter(ac=>~ac.types.indexOf('locality')) != undefined){
+            this.cottage.address.cityName = results[0].address_components.filter(ac=>~ac.types.indexOf('locality'))[0].long_name
+          }
+
+          if(results[0].address_components.filter(ac=>~ac.types.indexOf('country')) != undefined){
+            this.cottage.address.countryName = results[0].address_components.filter(ac=>~ac.types.indexOf('country'))[0].long_name
+          }
+
+          if( results[0].address_components.filter(ac=>~ac.types.indexOf('street_number')) != undefined){
+            this.cottage.address.streetNumber = results[0].address_components.filter(ac=>~ac.types.indexOf('street_number'))[0].long_name
+          }
+
+          if(results[0].address_components.filter(ac=>~ac.types.indexOf('route')) != undefined){
+            this.cottage.address.street = results[0].address_components.filter(ac=>~ac.types.indexOf('route'))[0].long_name
+          }
+
+          if(results[0].address_components.filter(ac=>~ac.types.indexOf('postal_code')) != undefined){
+            this.cottage.address.postalCode = results[0].address_components.filter(ac=>~ac.types.indexOf('postal_code'))[0].long_name
+          }
+
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
   }
 
 }
