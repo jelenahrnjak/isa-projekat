8248bf0d5a282f9ef453d4e4ce254b8a82482409
Jelena Hrnjak <jelenahrnjak99@gmail.com>
2022-06-03 20:10:33 +0200

    Feat/transactions student1
    * feat: added critical case for registration
    
    * feat: added transaction for canceling reservations
    
    * refactor: renamed methods

--- WishAndFish/src/main/java/com/example/WishAndFish/repository/ReservationRepository.java
+++ WishAndFish/src/main/java/com/example/WishAndFish/repository/ReservationRepository.java
@@ -1,12 +1,20 @@
 package com.example.WishAndFish.repository;
 
 import com.example.WishAndFish.model.Reservation;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
+
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select r from Reservation r where r.id = :id")
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value ="0")})
+    public Reservation findOneById(@Param("id")Long id);
 }
--- WishAndFish/src/main/java/com/example/WishAndFish/repository/UserRepository.java
+++ WishAndFish/src/main/java/com/example/WishAndFish/repository/UserRepository.java
@@ -4,9 +4,25 @@ import com.example.WishAndFish.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
+
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
     @Query("SELECT u FROM User u WHERE u.verificationCode = ?1")
     User findByVerificationCode(String code);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select u from User u")
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value ="0")})
+    List<User> findAllPessimistic();
+
 }
