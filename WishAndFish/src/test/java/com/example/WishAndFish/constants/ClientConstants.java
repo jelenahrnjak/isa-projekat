3588f6f57f2d1bdbbde2ec813d9616fa587ddd1f
jelenahrnjak <jelenahrnjak99@gmail.com>
2022-06-06 15:27:00 +0200

    feat: added tests for client subscriptions

---
+++ WishAndFish/src/test/java/com/example/WishAndFish/constants/ClientConstants.java
@@ -0,0 +1,18 @@
+package com.example.WishAndFish.constants;
+
+public class ClientConstants {
+
+    public static final Long DB_ID = 1L;
+    public static final String DB_PASS = "jelena";
+    public static final String DB_EMAIL = "jelena@gmail.com";
+    public static final String DB_NAME = "Jelena";
+    public static final String DB_SURNAME = "Hrnjak";
+    public static final String DB_PHONE = "0683920133";
+
+    public static final Long DB_BOAT_ID = 2l;
+    public static final Long DB_COTTAGE_ID = 3l;
+    public static final Long DB_ADVENTURE_ID = 3l;
+    public static final Long DB_BOAT_ID2 = 4l;
+    public static final Long DB_COTTAGE_ID2 = 5l;
+    public static final Long DB_ADVENTURE_ID2 = 6l;
+}
