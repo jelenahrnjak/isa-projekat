0319a03d95d5075ebdde54bbe5a6bc06cf8bb1f0
jelenahrnjak <jelenahrnjak99@gmail.com>
2022-06-06 16:02:52 +0200

    feat: added tests for subscribe and unsubscribe from cottage

--- WishAndFish/src/test/java/com/example/WishAndFish/service/ClientServiceTest.java
+++ WishAndFish/src/test/java/com/example/WishAndFish/service/ClientServiceTest.java
@@ -2,6 +2,7 @@ package com.example.WishAndFish.service;
 
 import com.example.WishAndFish.model.*;
 import com.example.WishAndFish.repository.ClientRepository;
+import com.example.WishAndFish.repository.CottageRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 import java.util.Optional;
 
 import static com.example.WishAndFish.constants.ClientConstants.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -23,6 +25,9 @@ public class ClientServiceTest {
 
     @Mock
     private ClientRepository clientRepositoryMock;
+
+    @Mock
+    private CottageRepository cottageRepositoryMock;
 
     @Mock
     private Client clientMock;
@@ -110,6 +115,63 @@ public class ClientServiceTest {
         verifyNoMoreInteractions(clientRepositoryMock);
     }
 
+
+    @Test
+    public void testUnsubscribeToCottage(){
+
+        User user = new User(DB_PASS, DB_EMAIL, DB_NAME, DB_SURNAME, DB_PHONE);
+        user.setId(DB_ID);
+
+        Client client = new Client(user);
+
+        Cottage cottage = new Cottage();
+        cottage.setId(DB_COTTAGE_ID);
+
+        Cottage cottage2 = new Cottage();
+        cottage2.setId(DB_COTTAGE_ID2);
+
+        client.getCottageSubscriptions().add(cottage);
+        client.getCottageSubscriptions().add(cottage2);
+
+        when(cottageRepositoryMock.getById(DB_COTTAGE_ID)).thenReturn(cottage);
+        when(clientRepositoryMock.findByEmail(DB_EMAIL)).thenReturn(client);
+
+        // 3. Verifikacija: asertacije i/ili verifikacija interakcije sa mock objektima
+        assertTrue(clientService.unsubscribeFromCottage(DB_COTTAGE_ID, DB_EMAIL));
+        assertFalse(clientService.checkCottageExistence(DB_EMAIL, DB_COTTAGE_ID));
+
+        verify(clientRepositoryMock, times(3)).findByEmail(DB_EMAIL);
+        verify(cottageRepositoryMock, times(1)).getById(DB_COTTAGE_ID);
+        verifyNoMoreInteractions(cottageRepositoryMock);
+    }
+
+    @Test
+    public void testSubscribeToCottage(){
+
+        User user = new User(DB_PASS, DB_EMAIL, DB_NAME, DB_SURNAME, DB_PHONE);
+        user.setId(DB_ID);
+
+        Client client = new Client(user);
+
+        Cottage cottage = new Cottage();
+        cottage.setId(DB_COTTAGE_ID);
+
+        Cottage cottage2 = new Cottage();
+        cottage2.setId(DB_COTTAGE_ID2);
+
+        client.getCottageSubscriptions().add(cottage);
+
+        when(cottageRepositoryMock.getById(DB_COTTAGE_ID2)).thenReturn(cottage2);
+        when(clientRepositoryMock.findByEmail(DB_EMAIL)).thenReturn(client);
+
+        // 3. Verifikacija: asertacije i/ili verifikacija interakcije sa mock objektima
+        assertTrue(clientService.subscribeToCottage(DB_COTTAGE_ID2, DB_EMAIL));
+        assertTrue(clientService.checkCottageExistence(DB_EMAIL, DB_COTTAGE_ID2));
+
+        verify(clientRepositoryMock, times(3)).findByEmail(DB_EMAIL);
+        verify(cottageRepositoryMock, times(1)).getById(DB_COTTAGE_ID2);
+        verifyNoMoreInteractions(cottageRepositoryMock);
+    }
 }
 
 
